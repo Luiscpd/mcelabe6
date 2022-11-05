14df594ce486c50de252ea261a16dcf864caee0c
Luiscpd <77604428+Luiscpd@users.noreply.github.com>
2022-11-05 05:43:21 -0600

    Delete fuseRelaunch.cmd

--- Medidor_Calidad_Energia/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "D:/Documentos/USAC/2022/Segundo Semestre 2022/Electronica 6/Laboratorio Electronica 6/Proyecto/Medidor_Calidad_Energia/TestTOP_isim_beh.exe" -prj "D:/Documentos/USAC/2022/Segundo Semestre 2022/Electronica 6/Laboratorio Electronica 6/Proyecto/Medidor_Calidad_Energia/TestTOP_beh.prj" "work.TestTOP" 
